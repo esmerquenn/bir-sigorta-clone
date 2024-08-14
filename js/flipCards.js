36f50db959b00d9b76ea36c043d6d7e0f683828d
esmerquenn <esmereyvazov@gmail.com>
2024-08-14 15:34:09 +0400

    okayyyy

--- js/flipCards.js
+++ js/flipCards.js
@@ -1,67 +1,158 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   const cardsData = [
+//     {
+//       id: 1,
+//       icon: "fa-hand-holding-heart",
+//       title: "Həyat Sığortası",
+//       description: "Həyat Sığortası online almaq artıq sizin üçün çox rahatdır",
+//       href: "heyat_sigorta.html",
+//     },
+//     {
+//       id: 2,
+//       icon: "fa-house-chimney",
+//       title: "Daşınmaz əmlakın icbari sığortası",
+//       description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero, quae?",
+//       href: "emlak.html",
+//     },
+//     {
+//       id: 3,
+//       icon: "fa-car",
+//       title: "İcbari Nəqliyyat sığortası",
+//       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste laborum",
+//       href: "avto.html",
+//     },
+//     {
+//       id: 4,
+//       icon: "fa-map",
+//       title: "Səyahət Sığortası",
+//       description: "Səyahət Sığortası online almaq artıq sizin üçün çox rahatdır",
+//       href: "seyahət.html",
+//     },
+
+//     {
+//       id: 6,
+//       icon: "fa-car",
+//       title: "KASKO Sığorta",
+//       description: "KASKO Sığorta haqqında məlumat",
+//       href: "kasko.html",
+//     },
+//   ];
+
+//   const cardsContainer = document.getElementById("cards-container");
+
+//   cardsData.forEach((card) => {
+//     const cardElement = document.createElement("div");
+//     cardElement.className = "flip-card shadow-lg rounded-lg";
+//     cardElement.innerHTML = `
+//             <div class="flip-card-inner">
+//                         <div id="card-${card.id}" class="flip-card-front bg-[rgb(2,1,142)] dark:bg-gray-800 text-white dark:text-gray-100 p-6 flex flex-col items-center justify-center rounded-lg">
+//                             <i class="fa-solid ${card.icon} text-[3rem] mb-5"></i>
+//                             <p class="text-2xl font-bold">${card.title}</p>
+//                         </div>
+//                         <div class="flip-card-back bg-[rgb(2,1,142)] dark:bg-gray-800 text-white dark:text-gray-100 p-6 flex flex-col items-center justify-center rounded-lg">
+//                             <p class="text-2xl font-bold">${card.title}</p>
+//                             <p class="py-4">${card.description}</p>
+//                             <a href='${card.href}' class="mt-4 py-2 px-4 bg-[rgb(2,1,142)] dark:bg-gray-800 text-white dark:text-gray-100 border-2 border-white dark:border-gray-600 rounded">Daha ətraflı</a>
+//                         </div>
+//                     </div>
+//         `;
+//     cardsContainer.appendChild(cardElement);
+//   });
+
+//   const cards = document.querySelectorAll(".flip-card");
+
+//   cards.forEach((card) => {
+//     card.addEventListener("mouseenter", () => {
+//       const inner = card.querySelector(".flip-card-inner");
+//       inner.style.transform = "rotateY(180deg)";
+//     });
+//     card.addEventListener("mouseleave", () => {
+//       const inner = card.querySelector(".flip-card-inner");
+//       inner.style.transform = "rotateY(0deg)";
+//     });
+//   });
+// });
+
 document.addEventListener("DOMContentLoaded", function () {
   const cardsData = [
     {
       id: 1,
       icon: "fa-hand-holding-heart",
       title: "Həyat Sığortası",
-      description: "Həyat Sığortası online almaq artıq sizin üçün çox rahatdır",
+      description: "Həyat sığortası ilə gələcəyinizi təhlükəsiz hala gətirin.",
       href: "heyat_sigorta.html",
+      dataKeyTitle: "life_insurance",
+      dataKeyDescription: "life_insurance_description",
     },
     {
       id: 2,
       icon: "fa-house-chimney",
       title: "Daşınmaz əmlakın icbari sığortası",
-      description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero, quae?",
+      description: "Daşınmaz əmlakınızı icbari sığorta ilə qoruyun.",
       href: "emlak.html",
+      dataKeyTitle: "property_insurance_mandatory",
+      dataKeyDescription: "property_insurance_description",
     },
     {
       id: 3,
       icon: "fa-car",
       title: "İcbari Nəqliyyat sığortası",
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste laborum",
+      description: "Nəqliyyat vasitələrinizi icbari sığorta ilə təmin edin.",
       href: "avto.html",
+      dataKeyTitle: "car_insurance_mandatory",
+      dataKeyDescription: "car_insurance_description",
     },
     {
       id: 4,
       icon: "fa-map",
       title: "Səyahət Sığortası",
-      description: "Səyahət Sığortası online almaq artıq sizin üçün çox rahatdır",
+      description: "Səyahət sığortası ilə rahat bir səfər planlayın.",
       href: "seyahət.html",
+      dataKeyTitle: "travel_insurance",
+      dataKeyDescription: "travel_insurance_description",
     },
-
     {
       id: 6,
       icon: "fa-car",
       title: "KASKO Sığorta",
-      description: "KASKO Sığorta haqqında məlumat",
+      description: "KASKO sığorta ilə avtomobilinizi təhlükələrdən qoruyun.",
       href: "kasko.html",
+      dataKeyTitle: "casco_insurance",
+      dataKeyDescription: "casco_insurance_description",
+    },
+    {
+      id: 5,
+      icon: "fa-briefcase-medical",
+      title: "Könüllü Tibbi Sığorta",
+      description: "Könüllü Tibbi Sığorta haqqında məlumat",
+      dataKeyTitle: "voluntary_health_insurance",
+      dataKeyDescription: "voluntary_health_insurance_description",
+    },
+    {
+      id: 7,
+      icon: "fa-truck",
+      title: "Yük sığortası",
+      description: "Yük sığortası haqqında məlumat",
+      dataKeyTitle: "cargo_insurance",
+      dataKeyDescription: "cargo_insurance_description",
+    },
+    {
+      id: 8,
+      icon: "fa-id-card",
+      title: "Yaşıl Kart Sistemi",
+      description: "Yaşıl Kart Sistemi haqqında məlumat",
+      dataKeyTitle: "green_card_system",
+      dataKeyDescription: "green_card_system_description",
+    },
+    {
+      id: 9,
+      icon: "fa-home",
+      title: "Daşınmaz Əmlakın Könüllü Sığortası",
+      description: "Daşınmaz Əmlakın Könüllü Sığortası haqqında məlumat",
+      dataKeyTitle: "voluntary_property_insurance",
+      dataKeyDescription: "voluntary_property_insurance_description",
     },
   ];
-
-  //   {
-  //     id: 5,
-  //     icon: "fa-briefcase-medical",
-  //     title: "Könüllü Tibbi Sığorta",
-  //     description: "Könüllü Tibbi Sığorta haqqında məlumat",
-  //   },
-  //   {
-  //     id: 7,
-  //     icon: "fa-truck",
-  //     title: "Yük sığortası",
-  //     description: "Yük sığortası haqqında məlumat",
-  //   },
-  //   {
-  //     id: 8,
-  //     icon: "fa-id-card",
-  //     title: "Yaşıl Kart Sistemi",
-  //     description: "Yaşıl Kart Sistemi haqqında məlumat",
-  //   },
-  //   {
-  //     id: 9,
-  //     icon: "fa-home",
-  //     title: "Daşınmaz Əmlakın Könüllü Sığortası",
-  //     description: "Daşınmaz Əmlakın Könüllü Sığortası haqqında məlumat",
-  //   },
 
   const cardsContainer = document.getElementById("cards-container");
 
@@ -69,18 +160,18 @@ document.addEventListener("DOMContentLoaded", function () {
     const cardElement = document.createElement("div");
     cardElement.className = "flip-card shadow-lg rounded-lg";
     cardElement.innerHTML = `
-            <div class="flip-card-inner">
-                        <div id="card-${card.id}" class="flip-card-front bg-[rgb(2,1,142)] dark:bg-gray-800 text-white dark:text-gray-100 p-6 flex flex-col items-center justify-center rounded-lg">
-                            <i class="fa-solid ${card.icon} text-[3rem] mb-5"></i>
-                            <p class="text-2xl font-bold">${card.title}</p>
-                        </div>
-                        <div class="flip-card-back bg-[rgb(2,1,142)] dark:bg-gray-800 text-white dark:text-gray-100 p-6 flex flex-col items-center justify-center rounded-lg">
-                            <p class="text-2xl font-bold">${card.title}</p>
-                            <p class="py-4">${card.description}</p>
-                            <a href='${card.href}' class="mt-4 py-2 px-4 bg-[rgb(2,1,142)] dark:bg-gray-800 text-white dark:text-gray-100 border-2 border-white dark:border-gray-600 rounded">Daha ətraflı</a>
-                        </div>
-                    </div>
-        `;
+              <div class="flip-card-inner">
+                  <div id="card-${card.id}" class="flip-card-front bg-[rgb(2,1,142)] dark:bg-gray-800 text-white dark:text-gray-100 p-6 flex flex-col items-center justify-center rounded-lg">
+                      <i class="fa-solid ${card.icon} text-[3rem] mb-5"></i>
+                      <p class="text-2xl font-bold" data-key="${card.dataKeyTitle}">${card.title}</p>
+                  </div>
+                  <div class="flip-card-back bg-[rgb(2,1,142)] dark:bg-gray-800 text-white dark:text-gray-100 p-6 flex flex-col items-center justify-center rounded-lg">
+                      <p class="text-2xl font-bold" data-key="${card.dataKeyTitle}">${card.title}</p>
+                      <p class="py-4" data-key="${card.dataKeyDescription}">${card.description}</p>
+                      <a href='${card.href}' class="mt-4 py-2 px-4 bg-[rgb(2,1,142)] dark:bg-gray-800 text-white dark:text-gray-100 border-2 border-white dark:border-gray-600 rounded" data-key="learn_more">Daha ətraflı</a>
+                  </div>
+              </div>
+          `;
     cardsContainer.appendChild(cardElement);
   });
 
